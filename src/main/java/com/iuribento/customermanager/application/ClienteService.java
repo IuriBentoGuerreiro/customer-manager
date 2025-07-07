6dabf1c8d6925602be2d40b14904ac1477c16cbd
Iuri Guerreiro <bentoiuri@gmail.com>
2025-07-07 18:11:28 -0300

    feat: create service

---
+++ src/main/java/com/iuribento/customermanager/application/ClienteService.java
@@ -0,0 +1,30 @@
+package com.iuribento.customermanager.application;
+
+import com.iuribento.customermanager.core.domain.Cliente;
+import com.iuribento.customermanager.core.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public List<Cliente> listarClientes(){
+        return clienteRepository.findAll();
+    }
+
+    public Cliente buscarClientePorId(UUID id){
+        return clienteRepository.findById(id).orElseThrow(
+                ()-> new RuntimeException("Cliente não encontrado")
+        );
+    }
+
+    public Cliente criarCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+}
