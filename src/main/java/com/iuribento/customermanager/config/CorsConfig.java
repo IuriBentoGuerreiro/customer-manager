b9189dd9da1fbd4ecf26ce38f956b360784e9fe7
Iuri Guerreiro <bentoiuri@gmail.com>
2025-07-08 01:28:38 -0300

    refactor: move ClienteService

---
+++ src/main/java/com/iuribento/customermanager/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.iuribento.customermanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private static final String PERMIT_ALL = "*";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(PERMIT_ALL)
+                .allowedOrigins(PERMIT_ALL)
+                .allowedHeaders(PERMIT_ALL)
+                .allowCredentials(false)
+                .maxAge(-1);
+    }
+}
